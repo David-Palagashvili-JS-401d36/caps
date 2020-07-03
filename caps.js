f6eda8e9295ffd235fb9f699ae179da01cf371d1
Davidoffili <palagashvilid@gmail.com>
2020-07-02 18:42:45 -0700

    added necessary files for modules, added white board, created an event emitter

---
+++ caps.js
@@ -0,0 +1,5 @@
+'use strict';
+// Main Hub Application
+// Manages the state of every package (ready for pickup, in transit, delivered, etc)
+// Logs every event to the console with a timestamp and the event payload
+// i.e. “EVENT {}”
