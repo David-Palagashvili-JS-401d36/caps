738d5b72ebe03b5dd732f2904dde421263761729
Davidoffili <palagashvilid@gmail.com>
2020-07-04 16:11:13 -0700

    removed old TODOs, let a simple string hold the store name instead of env

--- caps.js
+++ caps.js
@@ -1,9 +1,6 @@
 'use strict';
-// Main Hub Application
-
-// TODO: Logs every event to the console with a timestamp and the event payload
-// i.e. “EVENT {}”
-
+// Main Hub Application:
+// Manages the state of every package (ready for pickup, in transit, delivered, etc)
 const event = require('lib/events.js');
 
 require ('dotenv').config();
@@ -13,13 +10,13 @@ require('lib/vendor.js');
 // engaging our vendor module
 const runShipments = require('./lib/vendor.js');
 
-// Manages the state of every package (ready for pickup, in transit, delivered, etc)
+// The following listener, logs every event to the console with a timestamp & payload, then emits the trigger for the next event.
 event.on('package sorted for pick-up', (payload) => {
     let timeStamp = new Date();
     console.log( {EVENT: 'ready for pickup', timeStamp, payload} );
     event.emit('package scanned, assigned to driver', payload);
 });
-
+// Simulate a driver embarking on their route with the package on-board.
 event.on('in-transit', (payload) => {
     let timeStamp = new Date();
     console.log( {EVENT: 'in transit', timeStamp, payload} );
@@ -31,5 +28,5 @@ event.on('delivered', (payload) => {
     console.log( {EVENT: 'delivered', timeStamp, payload} );
     event.emit('delivered successfully', payload);   
 });
-// intiate operations by calling the function.
+// intiate shipping operations by calling our function.
 runShipments();
