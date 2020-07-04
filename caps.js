ebcddb3f3cadf7cfc860913e0be877d04f17ad33
Davidoffili <palagashvilid@gmail.com>
2020-07-03 18:57:57 -0700

    working on creating a fake order every 5 seconds, still soft testing create order function

--- caps.js
+++ caps.js
@@ -3,3 +3,8 @@
 // Manages the state of every package (ready for pickup, in transit, delivered, etc)
 // Logs every event to the console with a timestamp and the event payload
 // i.e. “EVENT {}”
+
+const event = require('lib/events.js');
+require ('dotenv').config();
+
+const shipPackage
