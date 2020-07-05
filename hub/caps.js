f89b124139de24cc9bcc92783d59b89a94aeb0ab
Davidoffili <palagashvilid@gmail.com>
2020-07-04 19:33:07 -0700

    completed handleData fn and fn logger

--- hub/caps.js
+++ hub/caps.js
@@ -1,5 +1,5 @@
 'use strict';
-require ('dotenv').config();
+require('dotenv').config();
 // Main Hub - CAPS Application Server:
 
 // It has one job which is to accept all inbound events and data, validate them, and and then re-broadcast them to everyone except the sender. 
@@ -32,14 +32,26 @@ server.on('error', (error) => {
     console.log(`SERVER ERROR found at:: ${error}`);
 });
 
-//On incoming data from a client
+// On incoming data from a client
 function handleData(buffer) {
-    let data = JSON.parse(buffer.toString());
-    console.log('cap server message', message);
-    if (message.event && message.payload) {
-      logger(message.payload);
-      for (let socket in socketPool) {
-        socketPool[socket].write(JSON.stringify(message));
-      };
+    let data = JSON.parse(buffer.toString()); //Read and parse the incoming payload
+    // console.log(data);
+    if (data.event && data.payload) { // If the payload is ok,
+        logger(data);
+        for (let socket in socketPool) { // broadcast the raw data back out:
+            socketPool[socket].write(JSON.stringify(data)); // to each of the other connected clients
+        };
     };
+    // console.log(data);
+};
+// logger for data objects coming through the hub. It should be a JSON object with both an event and payload properties.
+function logger(data) {
+    let event = data.event;
+    let timeStamp = new Date();
+    let payload = data.payload;
+    console.log({
+        event: event,
+        time: `${timeStamp}`,
+        payload: payload
+    });
 };
