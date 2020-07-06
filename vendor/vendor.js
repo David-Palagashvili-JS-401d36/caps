54d0fc8735cea28e2ee0a089761047fcff8f03ab
Davidoffili <palagashvilid@gmail.com>
2020-07-05 19:50:30 -0700

    dedicated fn validateEvent to check incoming events to log msg if delivered happens

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -15,37 +15,48 @@ Client.connect(3000, 'localhost', () => {
     console.log('Vendor has connected to Server');
 });
 
-function buildAnOrder() { // this function creates the order object
-    
-    let orderID = Math.ceil(Math.random() * 4000); // simulate a new customer via random order ID
+function buildAnOrder() { // this function creates the payload AKA order
+    let orderID = faker.random.number(); // simulate a new customer via random order ID
     let clientName = faker.name.findName();
     let clientAddress = faker.fake(
-        '{{client.streetAddress}}',
-        '{{client.city}}',
-        '{{client.state}}',
-        '{{client.zip}}'
+        '{{address.streetAddress}}',
+        '{{address.city}}',
+        '{{address.state}}',
+        '{{address.zip}}'
         ); // Create a fake order, as an object:
-    let fakeOrder = {
+    let shipmentData = {
         store: storeName,
         orderID: orderID,
         customer: clientName,
         address: clientAddress
-    }; // Emit a ‘pickup’ event and attach the fake order as payload
-    event.emit('package sorted for pick-up', fakeOrder);
+    }; // Create a message object with keys event and payload, carrying our order object.
+    Client.write(JSON.stringify({ event: 'pickup', payload: shipmentData }));
 };
 
 function assignToDriver() {
     let orderCount = 0;
     while(orderCount < 3) { // for testing, only makes 3 orders -- I will likely modify this in later phases
-        setTimeout(buildAnOrder, 5000); // Every 5 seconds, simulate a new fakeOrder
+        setTimeout(buildAnOrder, 5000); // Every 5 seconds, simulate new shipmentData
         orderCount = orderCount + 1;
     };
 };
 
+Client.on('data', validateEvent);
+
+function validateEvent(buffer) {
+    let data = JSON.parse(buffer.toString());
+    // console.log(data);
+    if (data.event === 'delivered') { // If the event is called delivered
+        console.log(`Order ${data.payload.orderID} was successfully delivered. Thank you!`)
+    }; // log a "thank you" msg
+};
+
+assignToDriver();
+
 // Whenever the ‘delivered’ event occurs, takes in payload and:
-event.on('delivered successfully', (payload) => { // Logs success message to the console
-    console.log(`Order ${payload.orderID} was successfully delivered. Thank you!`)
-});
+// event.on('delivered successfully', (payload) => { // Logs success message to the console
+//     console.log(`Order ${payload.orderID} was successfully delivered. Thank you!`)
+// });
 
 // export as module to use in caps
-module.exports = assignToDriver;
+// module.exports = assignToDriver;
